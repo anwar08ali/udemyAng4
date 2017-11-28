14d68c647e94de591d7b9922cd872552233b4fb7
Anwar <aali@zaloni.com>
2017-11-28 19:38:11 +0530

    removed console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent implements OnInit {
   title = 'app';
   //this variable to be used later when ngIf will be used to show hide login page in app.component.html
   isAuthenticated = false;
-  constructor(private authSrv: AuthenticationService){console.log("in con");};
+  constructor(private authSrv: AuthenticationService){
   isLoggedOut = new Subject<boolean>();
   ngOnInit(){
     this.authSrv.isLoggedOut.subscribe(
